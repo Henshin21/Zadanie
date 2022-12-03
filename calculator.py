004d24745b4b087033d09987da79f5da541f83ac
Tai Cao <Henshin21@yahoo.pl>
2022-12-03 13:39:56 +0100

    almost completed

--- calculator.py
+++ calculator.py
@@ -2,5 +2,30 @@ import sys
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
-def calculator():
-    
+math = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+if math == '1':
+    number = input("Podaj składnik 1.")
+    number_two = input("Podaj składnik 2.")
+    print(f"Dodaje {number} i {number_two} ")
+    sum = int(number) + int(number_two)
+    print(f"Wynik to {sum}")
+if math == '2':
+    number = input("Podaj składnik 1.")
+    number_two = input("Podaj składnik 2.")
+    print(f"Odejmuje {number} i {number_two} ")
+    diff = int(number) - int(number_two)
+    print(f"Wynik to {diff}")
+if math == '3':
+    number = input("Podaj składnik 1.")
+    number_two = input("Podaj składnik 2.")
+    print(f"Mnoże {number} i {number_two} ")
+    mult = int(number) * int(number_two)
+    print(f"Wynik to {mult}")
+if math == '4':
+    number = input("Podaj składnik 1.")
+    number_two = input("Podaj składnik 2.")
+    print(f"Dzielę {number} i {number_two} ")
+    div = int(number) / int(number_two)
+    print(f"Wynik to {div}")
+if math == 5:
+    exit(1)
