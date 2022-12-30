c2d2f23b7500b72491464d74d0143f9f60d0e15b
Tai Cao <Henshin21@yahoo.pl>
2022-12-30 15:02:24 +0100

    added faker library,added create_contacts function, minor fixes

--- wizytowki.py
+++ wizytowki.py
@@ -1,3 +1,7 @@
+from faker import Faker
+
+fake = Faker()
+
 class BaseContact():
     def __init__(self, name, surname, number, email):
         self.name = name
@@ -5,22 +9,30 @@ class BaseContact():
         self.number = number
         self.email = email
 
-    def contact():
-        print(f"Wybieram numer {number} i dzownie do {name} {surname}")
+    def contact(self):
+        print(f"Wybieram numer {self.number} i dzownie do {self.name} {self.surname}")
 
 class BusinessContact(BaseContact):
-    def __init__(self, position, company, business_number):
-        super().__init__(name, surname, email)
+    def __init__(self, name, surname, number, email, position, company):
+        super().__init__(name, surname, number, email)
         self.position = position
         self.company = company
-        self.business_number = business_number
 
-    def contact():
-        print(f"Wybieram numer {business_number} i dzownie do {name} {surname}")
+    def contact(self):
+        print(f"Wybieram numer {self.number} i dzownie do {self.name} {self.surname}({self.postion} w {self.comapny})")
     
     @property
-    def label_lenght():
-        return len(self.name, self.surname)
+    def label_lenght(self):
+        return len(self.name + self.surname)
 
-def create_contacts():
-    
+def create_contacts(contact_type, amount):
+    if contact_type == "base":
+        return [BaseContact(fake.first_name(), fake.last_name(),fake.number(),fake.email()) for x in range(amount)]
+    elif contact_type == "Business":
+        return [BusinessContact(fake.first_name(), fake.last_name(),fake.number(),fake.email(), fake.position(), fake.company()) for x in range(amount)]
+    else:
+        return []
+
+
+contacts = create_contacts("business", 4)
+
